94217e60b927d4c7192f9ebe8a14fe876a2a44bb
Geiger <geigereitan@gmail.com>
2016-12-15 15:20:18 +0200

    minor fixes to running with apache made logfile configureable

--- MiaLB/Mia.wsgi
+++ MiaLB/Mia.wsgi
@@ -3,4 +3,5 @@ import sys
 
 print sys.path
 
+from api_router import api_router as application
 #from . import *
--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-yum install -y httpd
+yum install -y httpd mod_wsgi
 semanage fcontext -a -t httpd_sys_content_t "/software/Mia/LB(/.*)?"
+semanage port -a -t http_port_t -p tcp 666
+semanage fcontext -a -t FILE_TYPE 'LB'
 mkdir -p /software/Mia/LB
 ( cd MiaLB ; tar -cf - . ) | ( cd /software/Mia/LB ; tar -xf - . )
 cp conf/apache-mia-lb.conf /etc/httpd/conf.d/mialb.conf
+cp conf/mialb.conf /software/Mia/LB/
+restorecon -R /software/Mia/
+chown apache:apache /software/Mia/LB
