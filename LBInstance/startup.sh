8fb820ccc1b53e0b950fd04780aeb8c5c07fed8b
lvgilmore <geigereitang@gmail.com>
2016-12-19 21:20:08 +0200

    mainly prepared the ground to add interface to the lb instance,   from the instance side

--- LBInstance/startup.sh
+++ LBInstance/startup.sh
@@ -4,13 +4,16 @@ uid=`hostname`
 default_interface=`ip -4 route show | grep default | sed 's/^.*\sdev\s\([^ ]*\)\s.*$/\1/'`
 my_ip=`ip -4 addr show ${default_interface} | grep -e "^\s*inet\s" | sed 's/^\s*inet\s\([0-9\.]*\)\/.*/\1/'`
 
-curl -H "Content-Type: application/json, Client-IP: ${my_ip}" -X POST -d '{"docker_uid": "'${uid}'"}' \
- "${MIALBURI}/MiaLB/farms/${FARMID}/instances"
 curl "${MIALBURI}/${FARMID}.conf" -o /etc/nginx/conf.d/${FARMID}.conf
+external_ip=$(grep -e "^[^\#]*proxy_pass " | sed 's/^.*proxy_pass \([0-9\.]*\)[: \s;].*$/\1/')
+curl -H "Content-Type: application/json, Client-IP: ${my_ip}" -X POST \
+  -d '{"docker_uid": "'${uid}'", "external_ip": ""}' \
+ "${MIALBURI}/MiaLB/farms/${FARMID}/instances"
+
 flag=1
 for i in {1..5}
 do
-    if ip addr show eth1 2> /dev/null
+    if [ 0 -eq $(ip addr show eth1 2> /dev/null; echo $?) ]
     then
         flag=0
         break
@@ -18,14 +21,15 @@ do
         sleep 5
     fi
 done
-if flag
+if [ $flag -eq 0 ]
 then
-    nginx
-    while flag
+    nginx -g "daemon off;"
+    while [ $flag -eq 0 ]
     do
         ps -ef | grep -v grep | grep -q nginx || flag=1
         sleep 5
     done
 else
     logger -s -p daemon.err "couldnt find eth1 after 25 seconds. aborting"
+    exit 1
 fi
---
+++ install_docker_host.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# parse arguments
+while [ $# -gt 0 ]
+do
+    case $1 in
+    '--token')
+        token=$2
+        shift 2 ;;
+    '--address')
+        address=$2
+        shift 2 ;;
+    *)
+        logger -p user.error "install docker host failed: argument error"
+        logger -p user.debug "$@"
+        exit 1
+    esac
+done
+
+# install docker
+cat > /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/fedora/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum install -y docker-engine
+sed -i 's@^ExecStart=.*$@ExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H 0.0.0.0:2376@' \
+  //usr/lib/systemd/system/docker.service
+systemctl daemon-reload
+systemctl enable docker.service ; systemctl start docker.service
+
+# join swarm cluster
+docker swarm join --token ${token} ${address}
+
+# set up networks
+brctl addbr br1
+docker network create --driver bridge
